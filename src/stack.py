3627e2116049ef8db8ecf8f9cdc58b136d9bcdde
dcamposliz <dcampos.liz@gmail.com>
2019-12-17 16:53:40 -0500

    Add gitignore and stack data structure class

---
+++ src/stack.py
@@ -0,0 +1,35 @@
+class Stack:
+    """
+    Stack (data structure) is an ordered collection of items where items
+    are added to and removed from the same end (known as top). LIFO.
+
+    Attributes:
+        items (int)
+    """
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        """
+        Return whether the stack is empty.
+        """
+        return self.items == []
+    
+    def push(self, item):
+        """
+        Add specified item to the top of the stack.
+        """
+        self.items.append(item)
+    
+    def pop(self):
+        """
+        Remove item from the top of the stack.
+        """
+        return self.items.pop()
+    
+    def peek(self):
+        """
+        Return item located at the top of the stack.
+        """
+        return self.items[len(self.items) - 1]
