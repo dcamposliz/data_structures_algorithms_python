9a67bb108398720ab20e70ac0489ea71e0acfb95
dcamposliz <dcampos.liz@gmail.com>
2019-12-18 02:26:50 -0500

    add ordered linked list

---
+++ src/OrderedLinkedList.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, initData):
+        self.data = initData
+        self.next = None
+    
+    def getData(self):
+        return self.data
+    
+    def getNext(self):
+        return self.next
+
+    def setData(self, newData):
+        self.data = newData
+
+    def setNext(self, newNext):
+        self.next = newNext
+
+class OrderedLinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def search(self, item):
+        current = self.head
+        found = False
+        stop = False
+        while current != None and not found and not stop:
+            if current.getData() == item:
+                found = True
+            else:
+                if current.getData() > item:
+                    stop = True
+                else:
+                    current = current.getNext()
+        return found
+
+    def add(self, item):
+        current = self.head
+        previous = None
+        stop = False
+        while current != None and not stop:
+            if current.getData() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+        temp = Node(item)
+        if previous == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(current)
+            previous.setNext(temp)
+        
+    # missing methods here
+    
