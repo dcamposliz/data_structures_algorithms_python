22af505223d7bc1be87f6fdcc608a412249cae5d
dcamposliz <dcampos.liz@gmail.com>
2019-12-18 01:32:59 -0500

    add more code to unordered linked list

--- src/UnorderedLinkedList.py
+++ src/UnorderedLinkedList.py
@@ -18,6 +18,7 @@ class Node:
     def setNext(self, newNext):
         self.next = newNext
 
+
 class UnorderedLinkedList:
     """
     An unordered linked list is a collection of items where each item
@@ -31,3 +32,44 @@ class UnorderedLinkedList:
         return self.head == None
     
     def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+    
+    def length(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+        return count
+    
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+        return found
+    
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+    
+    # def append()
+    # def insert()
+    # def index()
+    # def pop()
